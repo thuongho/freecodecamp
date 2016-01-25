923c7258b4438826827a992a355b60c6e7322423
Thuong Ho <thuong.t.ho@gmail.com>
2016-01-24 16:22:16 -0800

    update working solution to roman

--- intermediateAlgorithmScripting/i3_roman.js
+++ intermediateAlgorithmScripting/i3_roman.js
@@ -1,14 +1,4 @@
 function convert(num) {
-  var romanNum = {
-    one: "I",
-    five: "V",
-    ten: "X",
-    fifty: "L",
-    hundred: "C",
-    fiveHundred: "D",
-    thousand: "M"
-  };
-  
   var roman = {
     1: "I",
     5: "V",
@@ -19,64 +9,82 @@ function convert(num) {
     1000: "M"
   };
   
- var romanArr = [];
-  var numArr = num.toString().split('');
-  var numLen = numArr.length - 1;
+  var newRoman = {
+    1: "I",
+    2: "II",
+    3: "III",
+    4: "IV",
+    5: "V",
+    6: "VI",
+    7: "VII",
+    8: "VIII",
+    9: "IX",
+    10: "X",
+    20: "XX",
+    30: "XXX",
+    40: "XL",
+    50: "L",
+    60: "LX",
+    70: "LXX",
+    80: "LXXX",
+    90: "XC",
+    100: "C",
+    200: "CC",
+    300: "CCC",
+    400: "CD",
+    500: "D",
+    600: "DC",
+    700: "DCC",
+    800: "DCCC",
+    900: "CM",
+    1000: "M",
+    2000: "MM",
+    3000: "MMM"
+  };
+    // variables
+  var numArry = [];
+  var romanArr = [];
   
-  // 3999
-  // numArr = [ '3', '9', '9', '9' ]
-  // numLen = 4
-    while (numLen >= 0) {
-      romanArr.push(padChar(numArr.shift(),numLen, '0'));
-      numLen--;
+  // split 3999 to [ '3', '9', '9', '9' ]
+  numArry = num.toString().split('');
+  //$answer.text("numArry: " + numArry);
+  var length = numArry.length -1;
+  // add zeros to individual numbers
+  // 3000 900 90 9
+   
+  // iterate through and add zeros to the nums in array
+  for (var i =0; i < numArry.length; i ++) {
+    for (var y=0; y < length; y++) {
+      numArry[i] += '0';
     }
-  
-    function padChar(num, numTimes, charX) {
-      num = num.toString();
-      for (var i =0; i < numTimes; i++){
-        num = num.concat(charX);
-      }
-      //num = parseInt(num);
-      return num;
-    }
-  // romanArr = [ 3000, 900, 90, 9 ]
-  romanArr.forEach(breakDownNumToRoman);
-  
-  function breakDownNumToRoman(arg) {
-    // arg = 3000
-    var firstNum = arg[0],
-        romanType = '';
-    console.log(firstNum);
-    switch (firstNum) {
-      case "1":
-      case "5":
-        console.log(roman[arg]);
-        romanArr.push(roman[arg]);
-        break;
-      case "2":
-      case "3":
-        romanType = roman[padChar(1, arg.length -1, '0')];
-        console.log(romanType);
-        romanArr.push(padChar(romanType, firstNum -1, romanType));
-        break;
-      case "4":
-      case "9":
-        romanType = roman[1].concat(roman[firstNum + 1]);
-        console.log(romanType);
-        romanArr.push(romanType);
-        break;
-      case "6":
-      case "7":
-      case "8":
-        romanType = padChar(roman[5], firstNum - 5, roman[1]);
-        console.log(romanType);
-        romanArr.push(romanType);
-        break;
-    }
-    romanArr.shift();
-    console.log(romanArr);
+    romanArr.push(newRoman[numArry[i]]);
+    length--;
   }
  return romanArr.join("");
+  
+  // alternative base on length
+  // is numLen = 1
+  // indexOf (0 or numLen - numLen) is I or V or IX
+  
+  // is numLen = 2
+  // indexOf (0 or numLen - numLen) is X
+  // indexOf (1 or numLen - numLen-1) is I or V or IX
+  
+  // is numLen = 3
+  // indexOf (0 or numLen - numLen) is C
+  // indexOf (1 or numLen - numLen-1) is X or L or XC
+  // indexOf (2 or numLen - numLen-2) is I or V or IX
+  
+  // [3000,900,90,9]
+  // is numLen = 4
+  // indexOf (0 or numLen - numLen) is M
+  // 3000
+  // indexOf (1 or numLen - numLen-1) is C or D or CM
+  // 900
+  // indexOf (2 or numLen - numLen-2) is X or L or XC
+  // 90
+  // indexOf (3 or numLen - numLen-3) is I or V or IX
+  // 9
 }
 
-convert(36);
+convert(3999);
