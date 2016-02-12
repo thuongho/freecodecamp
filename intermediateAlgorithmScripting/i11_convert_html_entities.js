138163a0d58e1bdaa999c7a0b3badaf4e1c195d3
Thuong Ho <thuong.t.ho@gmail.com>
2016-02-12 10:53:51 -0800

    convert html entities

---
+++ intermediateAlgorithmScripting/i11_convert_html_entities.js
@@ -0,0 +1,9 @@
+function convert(str) {
+  // &colon;&rpar;
+ str = str.replace(/&/g, '&amp;').replace(/\"/g,'&quot;').replace(/</g,'&lt;').replace(/>/g, '&gt;').replace(/'/g, '&apos;');
+  
+  return str;
+}
+
+
+convert("Dolce & Gabbana");
