a5a5d3bef60ee86f3b5812cbabf81e5fd24e203f
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-07 23:48:03 -0800

    testing reduce and arguments

--- intermediateAlgorithmScripting/i10_sorted_union.js
+++ intermediateAlgorithmScripting/i10_sorted_union.js
@@ -1,5 +1,18 @@
 function unite(arr1, arr2, arr3) {
-  return arr1;
+  var args = Array.prototype.slice.call(arguments);
+  var c;
+
+  console.log(args);
+  args.reduce(function(previousValue, currentValue, currentIndex, array) {
+     c += previousValue + currentValue;
+     console.log('c',c);
+  }, args[0]);
 }
 
 unite([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+
+[0, 1, 2, 3, 4].reduce(function(previousValue, currentValue, currentIndex, array) {
+  return previousValue + currentValue;
+}, 10);
+
